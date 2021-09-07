d0bc402a7b9ebbcfd35b92bf4e81b2aeff56034d
hello-kozloff <hello.kozloff@gmail.com>
2021-09-07 18:56:21 +0300

    create home content

---
+++ src/router/components/Reducer/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+/**
+ * The home content.
+ * @return {React.ReactElement}
+ * @constructor
+ */
+export default function Home(): React.ReactElement {
+  return (
+    <h1>Welcome to home page!</h1>
+  );
+}
