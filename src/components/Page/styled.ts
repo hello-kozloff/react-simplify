0e8c9acd89ed84e3e0e3380a957aa77d356c72e5
hello-kozloff <hello.kozloff@gmail.com>
2021-09-08 19:01:37 +0300

    create common styled

---
+++ src/components/Page/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+/**
+ * The wrapper of the page.
+ */
+export const Wrapper = styled.div`
+
+`;
+
+/**
+ * The main in the page.
+ */
+export const Main = styled.main`
+
+`;
