2db28b6745e2b4732fcc7cf7a3d73ba4b406a4b1
hello-kozloff <hello.kozloff@gmail.com>
2021-09-08 20:32:56 +0300

    export app helpers

--- src/components/App/index.ts
+++ src/components/App/index.ts
@@ -2,3 +2,4 @@ export {default as App} from './App';
 export type {AppContext} from './types';
 export {Context} from './Context';
 export {useApp} from './hooks/useApp';
+export {default as getBrowserTheme} from './helpers/getBrowserTheme';
