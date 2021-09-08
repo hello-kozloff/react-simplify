f48c705801beb7b7237c70091b711e87af5e3304
hello-kozloff <hello.kozloff@gmail.com>
2021-09-08 20:26:40 +0300

    connect app context

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import {ContextProvider} from './Context';
+import {ThemeProvider, DefaultTheme} from 'styled-components';
 import {GlobalStyles} from './styled';
 import {Router, RouterReducer} from 'router';
 import {RouterProvider} from 'react-router5';
+import getBrowserTheme from './helpers/getBrowserTheme';
+import {AppContext} from './types';
 
 /**
  * The main app component.
@@ -9,12 +13,22 @@ import {RouterProvider} from 'react-router5';
  * @constructor
  */
 export default function App(): React.ReactElement {
+  const [
+    theme, setTheme,
+  ] = React.useState<DefaultTheme>(getBrowserTheme());
+
+  const context: AppContext = {
+    theme, setTheme,
+  };
+
   return (
-    <React.Fragment>
-      <GlobalStyles />
-      <RouterProvider router={Router.start()}>
-        <RouterReducer />
-      </RouterProvider>
-    </React.Fragment>
+    <ContextProvider {...context}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <RouterProvider router={Router.start()}>
+          <RouterReducer />
+        </RouterProvider>
+      </ThemeProvider>
+    </ContextProvider>
   );
 }
