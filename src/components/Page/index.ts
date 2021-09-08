d1b37d3a6c064bf165eaa652029bf7560a1046a6
hello-kozloff <hello.kozloff@gmail.com>
2021-09-08 19:02:15 +0300

    export page

---
+++ src/components/Page/index.ts
@@ -0,0 +1,4 @@
+export {default as Page} from './Page';
+export type {PageProps, PageContext} from './types';
+export {Context as Context} from './Context';
+export {usePage} from './hooks/usePage';
