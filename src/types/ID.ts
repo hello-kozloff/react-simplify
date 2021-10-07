92bd70bf12b25014e54b5554aaac0440926f37af
hello-kozloff <hello.kozloff@gmail.com>
2021-10-08 00:43:45 +0300

    create id type and object

---
+++ src/types/ID.ts
@@ -0,0 +1,19 @@
+/**
+ * ID type.
+ * @type {string}
+ */
+export type ID = string;
+
+/**
+ * The interface of object with id property.
+ * @param {ID} id
+ */
+export interface IDObject {
+
+  /**
+   * ID property.
+   * @type {ID}
+   */
+  id: ID;
+
+}
