393f427790a0deda660367ebb58b00310f6e3cc7
hello-kozloff <hello.kozloff@gmail.com>
2021-10-24 05:48:38 +0300

    create container component

---
+++ src/components/Container/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+/**
+ * The container wrapper.
+ * @interface ContainerProps
+ */
+export const Container = styled.div`
+  box-sizing: border-box;
+  padding: 0 16px;
+  margin: 0 auto;
+  max-width: 1180px;
+  width: 100%;
+`;
