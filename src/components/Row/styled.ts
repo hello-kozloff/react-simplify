d5730ddbe96828827577dc4dda365c26d664257e
hello-kozloff <hello.kozloff@gmail.com>
2021-10-24 05:42:20 +0300

    create row component

---
+++ src/components/Row/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+/**
+ * The row wrapper.
+ * @interface RowProps
+ */
+export const Row = styled.div`
+  padding: 8px 16px;
+  box-sizing: border-box;
+  grid-auto-flow: column;
+  display: grid;
+`;
